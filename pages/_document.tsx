050e89f2002c29071f0d19aebb83cff27f0e8dc0
Ameya Deokule <deokuleameya@gmail.com>
2021-06-05 20:31:30 +0530

    added document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import {Html, Head, Main, NextScript} from 'next/document'
+
+export default class CustomDocument extends Document{
+	render (){
+		return <Html>
+			<Head></Head>
+			<body>
+				<Main />
+			</body>
+			<NextScript />
+		</Html>
+	}
+}
